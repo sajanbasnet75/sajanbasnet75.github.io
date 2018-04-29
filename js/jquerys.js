0401ab1680017d4a7aca5bd3b98fe36c00418507
Sajan Basnet <sajanbasnet75@gmail.com>
2018-04-29 17:52:12 +0545

    personal

--- js/jquerys.js
+++ js/jquerys.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
  if($(window).width()<=768){
    $('#mob_jq2').insertBefore('#mob_jq')
  }
+
+
+$(".nav_list").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
 });
